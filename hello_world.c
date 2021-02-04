6ecf21d4a64588305eb746dd8c47c86a5e895f90
YashrajMohanty <75062038+YashrajMohanty@users.noreply.github.com>
2021-02-04 16:19:29 +0530

    Update hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -1,1 +1,10 @@
-//Write the program to print "Hello World !"
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+char name[20];
+printf("Enter your name:\n");
+scanf("%s",&name);
+printf("The name is: %s",name);
+}
