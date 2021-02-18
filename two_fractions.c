e8e1a3445129b339f6abad48023cc4a5c07e0bcd
Yashraj Mohanty <y45hofficial@gmail.com>
2021-02-18 11:11:57 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -1,1 +1,40 @@
-//WAP to find the sum of two fractions.
+#include<stdio.h>
+
+struct fraction
+{
+int x;
+int y;
+};
+
+void input(struct fraction *p)
+{
+printf("Enter the numerator:");
+scanf("%d",&(*p).x);
+printf("Enter the denominator:");
+scanf("%d",&(*p).y); 
+}
+
+int add(struct fraction n1,struct fraction n2)
+{
+int num,den;
+num=((n1.x)*(n2.y))+((n2.x)*(n1.y));
+den=(n1.y)*(n2.y);
+return num,den;
+}
+
+void output(int num,int den)
+{
+printf("The sum of the fractions is %d/%d",num,den);
+}
+
+void main()
+{
+struct fraction n1,n2;
+int num,den;
+printf("First fraction:\n");
+input(&n1);
+printf("Second fraction:\n");
+input(&n2);
+add(n1,n2);
+output(num,den);
+}
