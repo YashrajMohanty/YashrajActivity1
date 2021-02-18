3cf437d58c2b251590ba325ed6f67472baba2d19
Yashraj Mohanty <y45hofficial@gmail.com>
2021-02-18 11:33:48 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -11,15 +11,21 @@ void input(struct fraction *p)
 printf("Enter the numerator:");
 scanf("%d",&(*p).x);
 printf("Enter the denominator:");
-scanf("%d",&(*p).y); 
+scanf("%d",&(*p).y);
 }
 
-int add(struct fraction n1,struct fraction n2)
+int addnum(struct fraction n1,struct fraction n2)
 {
-int num,den;
+int num;
 num=((n1.x)*(n2.y))+((n2.x)*(n1.y));
+return num;
+}
+
+int addden(struct fraction n1,struct fraction n2)
+{
+int den;
 den=(n1.y)*(n2.y);
-return num,den;
+return den;
 }
 
 void output(int num,int den)
@@ -35,6 +41,7 @@ printf("First fraction:\n");
 input(&n1);
 printf("Second fraction:\n");
 input(&n2);
-add(n1,n2);
+num=addnum(n1,n2);
+den=addden(n1,n2);
 output(num,den);
 }
