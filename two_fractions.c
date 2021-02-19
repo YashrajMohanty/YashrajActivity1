4c056fb34b628423726e8252fe85e56256496c10
Yashraj Mohanty <y45hofficial@gmail.com>
2021-02-19 12:09:52 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -2,46 +2,88 @@
 
 struct fraction
 {
-int x;
-int y;
+   int x[100];
+   int y[100];
 };
 
-void input(struct fraction *p)
+int input(struct fraction *p)
 {
-printf("Enter the numerator:");
-scanf("%d",&(*p).x);
-printf("Enter the denominator:");
-scanf("%d",&(*p).y);
+   for(int i=0;i<2;i++)
+   {
+       printf("Enter numerator for fraction %d:",i+1);
+       scanf("%d",&(*p).x[i]);
+       printf("Enter denominator for fraction %d:",i+1);
+       scanf("%d",&(*p).y[i]);
+   }
 }
 
-int addnum(struct fraction n1,struct fraction n2)
+int addnum(struct fraction a)
 {
-int num;
-num=((n1.x)*(n2.y))+((n2.x)*(n1.y));
-return num;
+   int num,sumn=0;
+   for(int i=0;i<2;i++)
+   {
+      for(int j=0;j<2;j++)
+      {
+          if(j!=i)
+          {
+              a.x[i]=a.x[i]*a.y[j];
+          }
+          num=a.x[i];
+      }
+      sumn=sumn+num;
+   }
+   return sumn;
 }
 
-int addden(struct fraction n1,struct fraction n2)
+int addden(struct fraction a)
 {
-int den;
-den=(n1.y)*(n2.y);
-return den;
+   int den=1,sumd=0;
+   for(int i=0;i<2;i++)
+   {
+      den=den*(a.y[i]);
+      sumd=den;
+   }
+   return sumd;
 }
 
-void output(int num,int den)
+int gcd(int sumn,int sumd)
 {
-printf("The sum of the fractions is %d/%d",num,den);
+    int finalgcd,min,i;
+    if(sumn<=sumd)
+    {
+        min=sumn;
+    }
+    else
+    {
+        min=sumd;
+    }
+    for(i=2;i<=min;i++)
+    {
+        if(sumn%i==0 && sumd%i==0)
+        {
+            finalgcd=i;
+        }
+    }
+    return finalgcd;
+}
+
+void output(int sumn,int sumd,int finalgcd)
+{
+   printf("The sum of the fractions is %d/%d",sumn/finalgcd,sumd/finalgcd);
+}
+
+void add2frac()
+{
+   struct fraction a;
+   int sumn,sumd,finalgcd;
+   input(&a);
+   sumn=addnum(a);
+   sumd=addden(a);
+   finalgcd=gcd(sumn,sumd);
+   output(sumn,sumd,finalgcd);
 }
 
 void main()
 {
-struct fraction n1,n2;
-int num,den;
-printf("First fraction:\n");
-input(&n1);
-printf("Second fraction:\n");
-input(&n2);
-num=addnum(n1,n2);
-den=addden(n1,n2);
-output(num,den);
+    add2frac();
 }
