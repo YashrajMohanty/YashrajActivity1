90eb9581495f2bf75e46258a86f68ddeb7a77f1f
Yashraj Mohanty <y45hofficial@gmail.com>
2021-03-07 21:06:36 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -1,91 +1,53 @@
 #include<stdio.h>
-typedef struct 
-{
-    int n;
-    int d;
-}frac;
 
- 
-frac input();
-void compute(int);
-frac sum(frac,frac);
-int gcd(int,int);
-void output(frac);
- 
-void addfrac()
+struct Fract
 {
-    int n,i;
-    frac a,tot;
-    frac f[2];
-    compute(2);
-}
- 
-frac input()
+   int n,d;
+};
+
+struct Fract add(struct Fract, struct Fract);
+int gcd_calc(int,int);
+void output(struct Fract, struct Fract, struct Fract);
+
+int main()
 {
-    frac f;
-    printf("Enter the numerator:");
-    scanf("%d",&f.n);
-    printf("Enter the denominator:");
-    scanf("%d",&f.d);
-    return f;
-}
- 
-int gcd(int n, int d)
-{
-    int i,gcd;
-    for(i=1;i<=n && i<=d;i++)
-    {
-        if(n%i==0 && d%i==0)
-        {
-            gcd=i;
-        }
-    }
-    return gcd;
+   struct Fract a, b,c;
+   int sum,lcm;
+   printf("Enter 1st numerator and denominator:\n");
+   scanf("%d %d", &a.n, &a.d);
+   printf("Enter 2nd numerator and denominator:\n");
+   scanf("%d %d", &b.n, &b.d);
+   c=add(a,b);
+   output(a,b,c);
+   return 0;
 }
 
-void compute(int n)
+struct Fract add(struct Fract a, struct Fract b)
 {
-    int i;
-    frac tot,a,f[n];
-    for(i=0;i<n;i++)
-    {
-        printf("Fraction %d\n",i+1);
-        f[i]=input();
-    }
-    if(n==1)
-    {
-        tot=f[0];
-    }
-    else
-    {
-        a=f[0];
-        for(i=0;i<n-1;i++)
-        {
-            tot=sum(a,f[i+1]);
-            a=tot;
-        }
-    }
-    output(tot);
+   struct Fract c;
+   int gcd;
+   c.n=((a.n*b.d)+(b.n*a.d));
+   c.d=a.d*b.d;
+   gcd=gcd_calc(c.n,c.d);
+   c.n=c.n/gcd;
+   c.d=c.d/gcd;
+   return c;
 }
  
-frac sum(frac fract1, frac fract2)
+int gcd_calc(int d1,int d2)
 {
-    frac fract3;
-    int c;
-    fract3.n=((fract1.n*fract2.d)+(fract2.n*fract1.d));
-    fract3.d=fract1.d*fract2.d;
-    c=gcd(fract3.n,fract3.d);
-    fract3.n=fract3.n/c;
-    fract3.d=fract3.d/c;
-    return fract3;
-}
- 
-void output(frac total)
-{
-    printf("The sum of the fractions is %d / %d",total.n,total.d);
+   int i,gcd;
+   for(i=1;i<=d1&&i<=d2;i++)
+   {
+       if(d1%i==0&&d2%i==0)
+       {
+           gcd = i;
+       }
+   }
+   return gcd;
 }
 
-void main()
+void output(struct Fract a, struct Fract b,struct Fract c)
 {
-    addfrac();
+    printf("The sum of %d/%d and %d/%d is %d/%d",a.n,a.d,b.n,b.d,c.n,c.d);
 }
